57a9b8040872cdbacf8663a70cf7d275a0730ae8
athithan <athithya24@gmail.com>
2019-07-12 01:05:39 +0530

    Formatted code

--- src/test/java/com/testvagrant/testModules/HotelBookingTest.java
+++ src/test/java/com/testvagrant/testModules/HotelBookingTest.java
@@ -1,4 +1,5 @@
 package com.testvagrant.testModules;
+
 import java.io.File;
 
 import org.openqa.selenium.By;
@@ -16,36 +17,29 @@ import org.testng.annotations.Test;
 
 import com.testvagrant.pages.ClearTripPage;
 
-public class HotelBookingTest extends ClearTripPage{
+public class HotelBookingTest extends ClearTripPage {
 
-    
-	
 	@BeforeClass
-	public void loadApp()
-	{
+	public void loadApp() {
 		navigate("https://www.cleartrip.com/");
 		initElements();
 	}
 
-    @Test
-    public void shouldBeAbleToSearchForHotels() throws InterruptedException {
-    	
-    	hotelLink.click();
-    	localityTextBox.sendKeys(" Indiranagar, Bangalore");
-    	waitForElement(locationToSearch);
-    	locationToSeaerchSuggestion.click();
-    	selectValue(travellerSelection, "1 room, 2 adults");
-    	searchButton.click();
+	@Test
+	public void shouldBeAbleToSearchForHotels() throws InterruptedException {
 
+		hotelLink.click();
+		localityTextBox.sendKeys(" Indiranagar, Bangalore");
+		waitForElement(locationToSearch);
+		locationToSeaerchSuggestion.click();
+		selectValue(travellerSelection, "1 room, 2 adults");
+		searchButton.click();
 
-    }
-   
-    @AfterClass
-    public void windUp()
-    {
-    	driver.close();
-    }
+	}
 
-    
+	@AfterClass
+	public void windUp() {
+		driver.close();
+	}
 
 }
